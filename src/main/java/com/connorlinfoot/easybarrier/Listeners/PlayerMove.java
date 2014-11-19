443a73495327b01d8775315b97e19f1a486e4d30
Connor Linfoot <connorlinfoot@me.com>
2014-11-19 19:29:05 +0000

    Fixed above y only option

--- src/main/java/com/connorlinfoot/easybarrier/Listeners/PlayerMove.java
+++ src/main/java/com/connorlinfoot/easybarrier/Listeners/PlayerMove.java
@@ -21,20 +21,21 @@ public class PlayerMove implements Listener {
         Player player = event.getPlayer();
         String mat = EasyBarrier.getPlugin().getConfig().getString("Barrier Block");
         boolean aboveY = EasyBarrier.getPlugin().getConfig().getBoolean("Check Above Y Only");
-        Block closest = getBlockY(event.getTo(), new HashSet<Integer>(Arrays.asList(Material.getMaterial(mat).getId())), aboveY);
+        Block closest = getBlockY(event.getTo(), new HashSet<Integer>(Arrays.asList(Material.getMaterial(mat).getId())), aboveY, player.getLocation());
         if (closest != null) {
             event.setCancelled(true);
         }
     }
 
-    private static Block getBlockY(Location origin, Set<Integer> types, boolean aboveOnly) {
+    private static Block getBlockY(Location origin, Set<Integer> types, boolean aboveOnly, Location pLocation) {
         int x = origin.getBlockX();
         int y = origin.getBlockY();
         int z = origin.getBlockZ();
+        int py = pLocation.getBlockY();
         World world = origin.getWorld();
 
         for (int cy = 2; cy < 512; cy++) {
-            if (aboveOnly && cy < y) break;
+            if (aboveOnly && py < y) break;
             int testY;
             if ((cy & 1) == 0) {
                 testY = y + cy / 2;
