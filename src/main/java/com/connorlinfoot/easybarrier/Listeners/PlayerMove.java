86d520a5e0a67c818df430c85d5437526ad0f1d9
Connor Linfoot <connorlinfoot@icloud.com>
2014-11-19 14:32:25 +0000

    Debug

--- src/main/java/com/connorlinfoot/easybarrier/Listeners/PlayerMove.java
+++ src/main/java/com/connorlinfoot/easybarrier/Listeners/PlayerMove.java
@@ -13,6 +13,9 @@ public class PlayerMove implements Listener {
     public void onPlayerMove(PlayerMoveEvent event) {
         Player player = event.getPlayer();
         String mat = EasyBarrier.getPlugin().getConfig().getString("Barrier Block");
+        player.sendMessage("Event is being called");
+        player.sendMessage(mat);
+        player.sendMessage(String.valueOf(event.getTo().getBlock().getType()));
         if (event.getTo().getBlock().getType() == Material.getMaterial(mat) && event.getTo().add(0, 1, 0).getBlock().getType() == Material.getMaterial(mat)) {
             event.setCancelled(true);
         }
