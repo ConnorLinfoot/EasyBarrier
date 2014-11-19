848a7dd5e3fd506b8cccb4fa79ea909a288b0db9
Connor Linfoot <connorlinfoot@me.com>
2014-11-19 19:38:38 +0000

    Still trying to fix check above y option

--- src/main/java/com/connorlinfoot/easybarrier/Listeners/PlayerMove.java
+++ src/main/java/com/connorlinfoot/easybarrier/Listeners/PlayerMove.java
@@ -22,22 +22,21 @@ public class PlayerMove implements Listener {
         Player player = event.getPlayer();
         String mat = EasyBarrier.getPlugin().getConfig().getString("Barrier Block");
         boolean aboveY = EasyBarrier.getPlugin().getConfig().getBoolean("Check Above Y Only");
-        Block closest = getBlockY(event.getTo(), new HashSet<Integer>(Arrays.asList(Material.getMaterial(mat).getId())), aboveY, player.getLocation());
+        Block closest = getBlockY(event.getTo(), new HashSet<Integer>(Arrays.asList(Material.getMaterial(mat).getId())), aboveY);
         if (closest != null) {
             event.setCancelled(true);
         }
     }
 
-    private static Block getBlockY(Location origin, Set<Integer> types, boolean aboveOnly, Location pLocation) {
+    private static Block getBlockY(Location origin, Set<Integer> types, boolean aboveOnly) {
         int x = origin.getBlockX();
         int y = origin.getBlockY();
         int z = origin.getBlockZ();
-        int py = (int) pLocation.getY();
         World world = origin.getWorld();
 
         for (int cy = 2; cy < 512; cy++) {
-            if (aboveOnly && py < y) break;
-            Bukkit.broadcastMessage(py + " - " + y);
+            if (aboveOnly && y < cy) break;
+            Bukkit.broadcastMessage(y + " - " + cy);
             if (true) break;
             int testY;
             if ((cy & 1) == 0) {
